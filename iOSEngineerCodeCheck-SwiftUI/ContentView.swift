b6108f6da280b6e9ceba028a53f151e6a533bfee
den0206 <mac.pop@mineo.jp>
2020-12-22 19:14:37 +0900

    Initial Commit

---
+++ iOSEngineerCodeCheck-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSEngineerCodeCheck-SwiftUI
+//
+//  Created by 酒井ゆうき on 2020/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOSEngineerCodeCheck-SwiftUI/iOSEngineerCodeCheck_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSEngineerCodeCheck_SwiftUIApp.swift
+//  iOSEngineerCodeCheck-SwiftUI
+//
+//  Created by 酒井ゆうき on 2020/12/22.
+//
+
+import SwiftUI
+
+@main
+struct iOSEngineerCodeCheck_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
